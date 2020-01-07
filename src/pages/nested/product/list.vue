4c90616cefb160619b24713c66a41def677af51d
Your Namecyj1208 <283684087@qq.com>
2020-01-07 15:36:34 +0800

    cyj 第六次更新产品管理

--- src/pages/nested/product/list.vue
+++ src/pages/nested/product/list.vue
@@ -16,11 +16,16 @@
             width="55">
         </el-table-column>
             <el-table-column prop="id" label="编号"></el-table-column>
-            <el-table-column prop="name" label="产品名称"></el-table-column>
+            <el-table-column width="100px" prop="name" label="产品名称"></el-table-column>
             <el-table-column prop="price" label="价格"></el-table-column>
-            <el-table-column prop="description" label="描述"></el-table-column>
+            <el-table-column width="200px" prop="description" label="描述"></el-table-column>
             <el-table-column prop="categoryId" label="所属分类"></el-table-column>
-            <el-table-column label="操作">
+            <el-table-column label="产品图片">
+                <template slot-scope="scope">
+                    <img :src="scope.row.photo" width="200" height="200">
+                </template>
+            </el-table-column>
+            <el-table-column label="操作" f                                                                 ixed="right">
                 <template v-slot="slot">
                     <a href="" @click.prevent="toDeleteHandler(slot.row.id)"><i class="el-icon-delete"></i></a>
                     <a href="" @click.prevent="toUpdateHandler(slot.row)"><i class="el-icon-edit"></i></a>
@@ -37,7 +42,7 @@
             title="录入产品信息"
             :visible.sync="visible"
             width="60%">
-        ---{{form}}
+            ---{{form}}
             <el-form :model="form" label-width="80px">
                 <el-form-item label="产品名称">
                     <el-input v-model="form.name"></el-input>
@@ -58,7 +63,15 @@
                     <el-input v-model="form.description" type="textarea"></el-input>
                 </el-form-item>
                 <el-form-item label="产品主图">
-                    <el-input v-model="form.photo"></el-input>
+                    <el-upload
+                    class="upload-demo"
+                    action="http://134.175.154.93:6677/file/upload"
+                    :file-list="fileList"
+                    :on-success="uploadSuccessHandler"
+                    list-type="picture">
+                    <el-button size="small" type="primary">点击上传</el-button>
+                    <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+                    </el-upload>
                 </el-form-item>
             </el-form>      
             <span slot="footer" class="dialog-footer">
@@ -75,33 +88,39 @@ import querystring from 'querystring'
 export default {
     //methods用于存放网页中需要调用的方法
     methods:{
-      toggleSelection(rows) {
-        if (rows) {
-          rows.forEach(row => {
-            this.$refs.multipleTable.toggleRowSelection(row);
-          });
-        } else {
-          this.$refs.multipleTable.clearSelection();
-        }
-      },
-      handleSelectionChange(val) {
-        this.multipleSelection = val;
-      },
- 
-         loadCategory(){
+        //上传成功的事件处理函数
+        uploadSuccessHandler(response){
+            let photo = "http://134.175.154.93:8888/group1/"+response.data.id
+            //将图片地址设置到form中，便于一起提交到后台
+            this.form.photo = photo;
+        },
+        toggleSelection(rows) {
+            if (rows) {
+            rows.forEach(row => {
+                this.$refs.multipleTable.toggleRowSelection(row);
+            });
+            } else {
+            this.$refs.multipleTable.clearSelection();
+            }
+        },
+        handleSelectionChange(val) {
+            this.multipleSelection = val;
+        },
+        //加载栏目信息
+        loadCategory(){
             let url = "http://localhost:6677/category/findAll"
-      request.get(url).then((response)=>{
-        // 将查询结果设置到customers中，this指向外部函数的this
-        this.options = response.data;
-      })
-    },
+            request.get(url).then((response)=>{
+            // 将查询结果设置到customers中，this指向外部函数的this
+            this.options = response.data;
+        })
+        },
         loadData(){
             let url = "http://localhost:6677/product/findAll"
-      request.get(url).then((response)=>{
-        // 将查询结果设置到customers中，this指向外部函数的this
-        this.products = response.data;
-      })
-    },
+            request.get(url).then((response)=>{
+            // 将查询结果设置到customers中，this指向外部函数的this
+            this.products = response.data;
+        })
+        },
         //通过request与后台进行交互，并且携带参数
         submitHandler(){
             //交互端口
@@ -137,7 +156,7 @@ export default {
             request.get(url).then((response)=>{
                 //刷新数据
                 this.loadData();
-                //提示结果
+                //提示结果c
                 this.$message({
                 type: 'success',
                 message: response.message
@@ -152,6 +171,7 @@ export default {
         },
         toUpdateHandler(row){
             //
+            this.fileList = [];
             this.form=row;
             this.visible=true;
         },
@@ -160,8 +180,9 @@ export default {
         },
         toAddHandler(){
             //将form变为初始值
-             this.form={}
-           this.visible=true;
+            this.fileList = [];
+            this.form={}
+            this.visible=true;
         }
     },
     //用于存放要向网页中显示的数据
@@ -170,8 +191,8 @@ export default {
             visible:false,
             products:[],
             options:[],
-            form:{}
-            
+            form:{},
+            fileList:{}
         }
     },
     //created表示vue实例创建完毕，准备执行
